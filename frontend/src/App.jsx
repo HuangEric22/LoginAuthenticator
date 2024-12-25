c931dd514032d44333caaec8f7f580e5797dfed3
HuangEric22 <huangeric50@gmail.com>
2024-12-25 05:58:25 -0800

    Installed front end dependencies and also created homepage for login

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import FloatingShape from "./components/FloatingShape";
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className='min-h-screen bg-gradient-to-br 
+    from-gray-950 via-sky-900 to-cyan-900 flex items-center justify-center relative overflow-hidden'>
+      <h1 className='text-yellow-500 text-5xl'>hello world</h1>
+      <FloatingShape color ="bg-sky-600" size="w-64 h-64"  top="-5%" left="10%" delay={0}   />
+      <FloatingShape color ="bg-cyan-500" size="w-48 h-48"  top="70%" left="80%" delay={5}   />
+      <FloatingShape color ="bg-blue-300" size="w-32 h-32"  top="40%" left="-8%" delay={2}   />
+
+      <Routes>
+
+      </Routes>
+    </div>
+  )
+}
+
+export default App;
